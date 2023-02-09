de6472f7ed21908e7fd4d53a31edba7507ad3ad6
NotHydra <rizky.irswanda115@gmail.com>
2023-02-09 17:35:57 +0800

    feat: nav brand adjuts

--- sources/components/nav.php
+++ sources/components/nav.php
@@ -25,7 +25,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4" id="aside-theme" style="height: 100vh;">
     <a href="/<?php echo $originalPath; ?>" class="brand-link">
         <img src="<?php echo $sourcePath; ?>/public/dist/img/app-logo.png" alt="App Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Pelaporan</span>
+        <span class="brand-text font-weight-light">Pembayaran SPP</span>
     </a>
 
     <div class="sidebar">
