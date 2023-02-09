d181b31be66476eb6f51c92b83805245ee509e07
NotHydra <rizky.irswanda115@gmail.com>
2023-02-09 21:07:46 +0800

    feat: spp

---
+++ sources/models/spp/hapus.php
@@ -0,0 +1,122 @@
+<?php
+$sourcePath = "../..";
+include "$sourcePath/utilities/environment.php";
+include "$sourcePath/utilities/connection.php";
+include "$sourcePath/utilities/session/start.php";
+
+include "$sourcePath/middlewares/isNotAuthenticated.php";
+
+include "$sourcePath/utilities/session/data.php";
+include "$sourcePath/utilities/role.php";
+include "$sourcePath/utilities/date.php";
+include "$sourcePath/utilities/currency.php";
+
+roleGuardMinimum($sessionLevel, "admin", "/$originalPath");
+
+$id = $_GET["id"];
+$result = mysqli_query($connection, "SELECT id FROM spp WHERE id='$id' AND dihapus='0';");
+if (mysqli_num_rows($result) <= 0) {
+  echo "<script>window.location='.';</script>";
+};
+?>
+
+<!DOCTYPE html>
+<html lang="in">
+
+<head>
+  <?php
+  $headTitle = "SPP";
+  include "$sourcePath/components/head.php";
+  include "$sourcePath/components/select/head.php";
+  include "$sourcePath/utilities/modal.php";
+  ?>
+</head>
+
+<body class="hold-transition layout-navbar-fixed layout-fixed light-mode" id="body-theme">
+  <div class="wrapper">
+    <?php
+    $navActive = [5, null];
+    include "$sourcePath/components/nav.php";
+    ?>
+
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm">
+              <div class="card">
+                <?php
+                $pageItemObject = $pageArray[$navActive[0]];
+                $extraTitle = "Hapus";
+                include "$sourcePath/components/content/head.php";
+                ?>
+
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm">
+                      <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST" onsubmit="return confirmModal('form', this);">
+                        <?php
+                        $data = mysqli_fetch_assoc(mysqli_query($connection, "SELECT tahun, nominal FROM spp WHERE id='$id' AND dihapus='0';"));
+                        $inputArray = [
+                          [
+                            "id" => 1,
+                            "display" => "Tahun",
+                            "name" => "tahun",
+                            "type" => "number",
+                            "value" => $data["tahun"],
+                            "placeholder" => "Masukkan tahun disini",
+                            "enable" => false
+                          ],
+                          [
+                            "id" => 1,
+                            "display" => "Nominal",
+                            "name" => "nominal",
+                            "type" => "text",
+                            "value" => numberToCurrency($data["nominal"]),
+                            "placeholder" => "Masukkan nominal disini",
+                            "enable" => false
+                          ]
+                        ];
+
+                        include "$sourcePath/components/input/detail.php";
+                        ?>
+
+                        <button class="btn btn-danger btn-block" type="submit"><i class="fa fa-trash"></i> Hapus</button>
+                        <a class="btn btn-danger btn-block" role="button" onclick="confirmModal('location', '.');"><i class="fa fa-undo"></i> Kembali</a>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    include "$sourcePath/components/footer.php";
+    ?>
+  </div>
+
+  <?php
+  include "$sourcePath/components/script.php";
+  include "$sourcePath/components/select/script.php";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+      $result = mysqli_query($connection, "UPDATE tingkat SET dihapus='1' WHERE id='$id' AND dihapus='0';");
+
+      if ($result) {
+        echo "<script>successModal(null, '.');</script>";
+      } else {
+        echo "<script>errorModal(null, null);</script>";
+      };
+    } catch (exception $e) {
+      echo "<script>errorModal(null, null);</script>";
+    };
+  };
+  ?>
+</body>
+
+</html>
