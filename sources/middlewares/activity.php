a8c6d1c930f9a7cf5fbdbc76616d6df48574de84
NotHydra <rizky.irswanda115@gmail.com>
2023-02-14 14:14:27 +0800

    fix: activity only for petugas

--- sources/middlewares/activity.php
+++ sources/middlewares/activity.php
@@ -1,6 +1,8 @@
 <?php
 function activity($aktivitas)
 {
-    global $connection, $sessionId;
-    mysqli_query($connection, "INSERT INTO aktivitas (id_petugas, aktivitas) VALUES ('$sessionId', '$aktivitas');");
-}
+    global $connection, $sessionId, $sessionType;
+    if ($sessionType == "petugas") {
+        mysqli_query($connection, "INSERT INTO aktivitas (id_petugas, aktivitas) VALUES ('$sessionId', '$aktivitas');");
+    };
+};
