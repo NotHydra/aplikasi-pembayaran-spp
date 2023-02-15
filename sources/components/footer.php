eb02d33c77fc20ec0b4c0a70b0521aa1f89cc765
NotHydra <rizky.irswanda115@gmail.com>
2023-02-15 21:19:01 +0800

    feat: spp detail schema

--- sources/components/footer.php
+++ sources/components/footer.php
@@ -2,6 +2,6 @@
     <b>Copyright © <?php echo currentYear(); ?> Rizky Irswanda Ramadhana</b>
 
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version Alpha</b>
+        <b>Version Beta</b>
     </div>
 </footer>
--- sources/utilities/environment.php
+++ sources/utilities/environment.php
@@ -1,8 +1,8 @@
 <?php
-$originalPath = "programs/12%20RPL/PWPB/Project/aplikasi-pembayaran-spp";
+$originalPath = "programs/12%20RPL/PWPB/Project/aplikasi-pembayaran-spp-v2";
 $timezone = "Asia/Hong_Kong";
 
 $host = "localhost";
 $username = "root";
 $password = "Irsw4nd410102005";
-$database = "pwpb_aplikasi_pembayaran_spp";
+$database = "pwpb_aplikasi_pembayaran_spp_v2";
