5619f322a5f68e0ade5fc17e68fef80aa207b475
NotHydra <rizky.irswanda115@gmail.com>
2023-02-17 08:58:58 +0800

    feat: content head extra title loop

--- sources/components/content/head.php
+++ sources/components/content/head.php
@@ -6,13 +6,27 @@
 
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/<?php echo $originalPath; ?>/<?php echo $pageItemObject["link"]; ?>"><?php echo $pageItemObject["title"]; ?></a></li>
+                <li class="breadcrumb-item">
+                    <a href="/<?php echo $originalPath; ?>/<?php echo $pageItemObject["link"]; ?>"><?php echo $pageItemObject["title"]; ?></a>
+                </li>
 
                 <?php
                 if ($extraTitle != null) {
+                    foreach ($extraTitle as $extraTitleObject) {
                 ?>
-                    <li class="breadcrumb-item active"><?php echo $extraTitle; ?></li>
+                        <li class="breadcrumb-item active">
+                            <?php
+                            if ($extraTitleObject["link"] != null) {
+                            ?>
+                                <a href="/<?php echo $originalPath; ?>/<?php echo $extraTitleObject["link"]; ?>"><?php echo $extraTitleObject["title"]; ?></a>
+                            <?php
+                            } else {
+                                echo $extraTitleObject["title"];
+                            }
+                            ?>
+                        </li>
                 <?php
+                    }
                 };
                 ?>
             </ol>
