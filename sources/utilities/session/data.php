cb7560029baec37467330daddfc04821e37b6491
NotHydra <rizky.irswanda115@gmail.com>
2023-03-14 23:02:14 +0800

    fix: session middleware error where petugas does not exist

--- sources/utilities/session/data.php
+++ sources/utilities/session/data.php
@@ -3,19 +3,24 @@ $sessionId = $_SESSION["id"];
 $sessionType = $_SESSION["type"];
 
 if ($sessionType == "petugas") {
-    $result = mysqli_query($connection, "SELECT nama, username, telepon, level FROM petugas WHERE id='$sessionId' AND status='aktif';");
+    $result = mysqli_query($connection, "SELECT nama, username, telepon, level, status FROM petugas WHERE id='$sessionId';");
 
     if (mysqli_num_rows($result) > 0) {
         $data = mysqli_fetch_assoc($result);
 
-        $sessionNama = $data["nama"];
-        $sessionUsername = $data["username"];
-        $sessionTelepon = $data["telepon"];
+        if ($data["status"] == "aktif") {
+            $sessionNama = $data["nama"];
+            $sessionUsername = $data["username"];
+            $sessionTelepon = $data["telepon"];
 
-        $sessionLevel = $data["level"];
-        $sessionStatus = "aktif";
+            $sessionLevel = $data["level"];
+            $sessionStatus = "aktif";
+        } else {
+            echo "<script>window.location='/$originalPath/sources/models/authentication/logout.php'</script>";
+        }
     } else {
-        echo "<script>window.location='/$originalPath/sources/models/authentication/logout.php'</script>";
+        session_destroy();
+        echo "<script>window.location='/$originalPath'</script>";
     };
 } else if ($sessionType == "siswa") {
     $result = mysqli_query($connection, "SELECT nisn, nis, nama, id_rombel, alamat, telepon FROM siswa WHERE id='$sessionId';");
