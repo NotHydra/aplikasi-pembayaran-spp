8e803e634ba4a13f69f6046831c55510ff6c695b
NotHydra <rizky.irswanda115@gmail.com>
2023-03-27 09:58:31 +0800

    feat: no wrap to td

--- sources/components/head.php
+++ sources/components/head.php
@@ -6,3 +6,9 @@
  <link rel="stylesheet" href="<?php echo $sourcePath; ?>/public/plugins/fontawesome-free/css/all.min.css">
  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
  <link rel="stylesheet" href="<?php echo $sourcePath; ?>/public/dist/css/adminlte.min.css">
+
+ <style>
+     td {
+         white-space: nowrap;
+     }
+ </style>
